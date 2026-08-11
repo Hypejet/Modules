d5a711459145a617b28132f879ad98609b9a4d0e
codestech <56263050+Codestech1@users.noreply.github.com>
2026-08-11 12:11:10 +0200

    chore(gradle): increment the project version to `1.0.4-SNAPSHOT`

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 val javaVersion = 25
 
 group = "net.hypejet"
-version = releaseTag() ?: "1.0.3-SNAPSHOT"
+version = releaseTag() ?: "1.0.4-SNAPSHOT"
 description = "A Java library making modular programming easier"
 
 repositories {
